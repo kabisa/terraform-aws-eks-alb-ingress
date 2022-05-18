192c50573d8677cecda09c8fdcff9fd92c173192
Glaaj <83582518+Glaaj@users.noreply.github.com>
2022-05-18 16:52:51 +0200

    Adds descriptions to vars and outputs

--- variables.tf
+++ variables.tf
@@ -1,17 +1,24 @@
 variable "eks_cluster_name" {
   type = string
+  description = "The name of the EKS cluster."
 }
 
 variable "region" {
   type = string
+  description = "The AWS region."
 }
 
 variable "oidc_host_path" {
   type = string
+  description = "The host path of the OIDC provider."
 }
 
-variable "vpc_id" {}
+variable "vpc_id" {
+  type = string
+  description = "The VPC ID."
+}
 
 variable "account_id" {
   type = string
+  description = "The AWS account ID."
 }
